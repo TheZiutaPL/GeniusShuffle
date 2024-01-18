babe516fb88201b109849d6b273e394b54e77fb9
NorbertCode <54719382+NorbertCode@users.noreply.github.com>
2024-01-18 19:46:33 +0100

    Added some visual effects to toggle

--- Assets/Scripts/Menu/MenuToggle.cs
+++ Assets/Scripts/Menu/MenuToggle.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// A simple on/off toggle button
+
 public class MenuToggle : InteractionHandler
 {
     public bool isOn = false;
+    [SerializeField] GameObject display;
+
+    [Header("Click animation settings")]
+    [SerializeField] PointBasedMovement buttonObject;
+    [SerializeField] Transform onPosition;
+    [SerializeField] Transform offPosition;
 
     public override void ClickAction()
     {
@@ -15,5 +23,9 @@ public class MenuToggle : InteractionHandler
     public void SetStatus(bool isOn)
     {
         this.isOn = isOn;
+
+        display?.SetActive(isOn);
+        if (onPosition != null && onPosition != null)
+            buttonObject?.ForceMoveTo(isOn ? onPosition : offPosition);
     }
 }
