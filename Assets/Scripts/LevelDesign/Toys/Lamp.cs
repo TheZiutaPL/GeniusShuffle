c5aac348cd4acfac5744e348cb1470100261b0ce
Karol Sitkiewicz <flawarestudios@gmail.com>
2024-02-23 16:49:04 +0100

    Lamp improvements

--- Assets/Scripts/LevelDesign/Toys/Lamp.cs
+++ Assets/Scripts/LevelDesign/Toys/Lamp.cs
@@ -4,24 +4,32 @@ using UnityEngine;
 
 public class Lamp : MonoBehaviour
 {
+    [Header("Light")]
     [SerializeField] private GameObject lightObject;
     [SerializeField] private bool activeOnStart;
     private bool active;
 
+    [Header("Switch")]
+    [SerializeField] private Transform switchTransform;
+    [SerializeField] private float tiltXDegees = 15;
+    private Vector3 switchStartLocalRotation;
+
     private void Awake()
     {
+        switchStartLocalRotation = switchTransform.localEulerAngles;
+
         ToggleLight(activeOnStart);
     }
 
-    public void ToggleLight()
-    {
-        active = !active;
-        lightObject.SetActive(active);
-    }
+    public void ToggleLight() => ToggleLight(!active);
 
     public void ToggleLight(bool toggle)
     {
         active = toggle;
         lightObject.SetActive(active);
+
+        UpdateSwitch();
     }
+
+    private void UpdateSwitch() => switchTransform.localEulerAngles = new Vector3(switchStartLocalRotation.x + (active ? tiltXDegees : -tiltXDegees), switchStartLocalRotation.y, switchStartLocalRotation.z);
 }
